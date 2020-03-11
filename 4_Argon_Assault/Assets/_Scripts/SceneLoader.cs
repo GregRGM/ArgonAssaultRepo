3572e0a84d63ebe298de52a4b0ee25c70959316f
GregRGM <g.morton@digipen.edu>
2020-03-10 19:40:51 -0700

    Reorganized folders & names. Fixed lasers. Added Generic enemies and obstacle prefabs.

---
+++ 4_Argon_Assault/Assets/_Scripts/SceneLoader.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    // Start is called before the first frame update
+    // Use this for initialization
+    void Start()
+    {
+        Invoke("LoadFirstScene", 2f);
+    }
+
+    void LoadFirstScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
+
